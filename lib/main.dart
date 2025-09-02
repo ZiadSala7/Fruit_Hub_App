b88bfb5084a598eb789c0cd645400b5f7b81566a
Ziad <zslah2800@gmail.com>
2025-09-02 18:19:23 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitHubApp());
+}
+
+class FruitHubApp extends StatelessWidget {
+  const FruitHubApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
